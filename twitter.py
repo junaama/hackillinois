6e43ad48dc0742e9d1de468e78202f7defce5e92
aparnaayyah <aayyah2@illinois.edu>
2021-04-09 21:37:20 -0500

    added predictions

--- twitter.py
+++ twitter.py
@@ -1,24 +1,19 @@
+#twitter and requests for data scraping
 import twitter
 import requests
+#pd for holding the requested tweets
 import pandas as pd
+#flair for sentiment analysis model
 import flair
+#re for regex preprocessing
+import re
 
-print("hello world")
+#------------------------------------------------------------------------------#
 
-params = {
-    'q': 'tesla',
-    'tweet_mode': 'extended',
-    'lang': 'en',
-    'count': '100'
-}
+#PART 1: GETTING TWITTER DATA
 
-response = requests.get(
-    'https://api.twitter.com/1.1/search/tweets.json',
-    params=params,
-    headers={'authorization': 'Bearer '+ "AAAAAAAAAAAAAAAAAAAAAGvSOQEAAAAAeMnU6cOK5qNAaUl4g3jIltH0nNw%3DZTvb9om1pZAkDkZmmvI2ZxldPQiTSyMg8ulk5jH1d9rONo4N2o"}
-)
-
-def get_data(tweet):
+#functions to use
+def getData(tweet):
     data = {
         'id': tweet['id_str'],
         'created_at': tweet['created_at'],
@@ -27,13 +22,80 @@ def get_data(tweet):
     return data
 
 
+#make a request for tweets mentioning "tesla"
+params = {
+    'q': 'tesla',
+    'tweet_mode': 'extended',
+    'lang': 'en',
+    'count': '1000'
+}
+response1 = requests.get(
+    'https://api.twitter.com/1.1/search/tweets.json',
+    params=params,
+    headers={'authorization': 'Bearer '+ "AAAAAAAAAAAAAAAAAAAAAGvSOQEAAAAAeMnU6cOK5qNAaUl4g3jIltH0nNw%3DZTvb9om1pZAkDkZmmvI2ZxldPQiTSyMg8ulk5jH1d9rONo4N2o"}
+)
+
+#make a request for tweets mentioning "tsla"
+params = {
+    'q': 'tsla',
+    'tweet_mode': 'extended',
+    'lang': 'en',
+    'count': '1000'
+}
+response2 = requests.get(
+    'https://api.twitter.com/1.1/search/tweets.json',
+    params=params,
+    headers={'authorization': 'Bearer '+ "AAAAAAAAAAAAAAAAAAAAAGvSOQEAAAAAeMnU6cOK5qNAaUl4g3jIltH0nNw%3DZTvb9om1pZAkDkZmmvI2ZxldPQiTSyMg8ulk5jH1d9rONo4N2o"}
+)
+
+#adds response1 and response2 tweets to the dataframe (df)
 df = pd.DataFrame()
-for tweet in response.json()['statuses']:
-    row = get_data(tweet)
+for tweet in response1.json()['statuses']:
+    row = getData(tweet)
     df = df.append(row, ignore_index=True)
 
-sentiment_model = flair.models.TextClassifier.load('en-sentiment')
+for tweet in response2.json()['statuses']:
+    row = getData(tweet)
+    df = df.append(row, ignore_index=True)
 
-sentence = flair.data.Sentence("Gamestop is going to the moon!")
-sentiment_model.predict(sentence)
-print(sentence)
+#tweets now holds all of our relavant tweets
+tweets = df
+
+#------------------------------------------------------------------------------#
+
+#PART 2: PREPROCESSING
+#we're using RegEx library re to remove whitespace, 
+
+# preprocessing to remove whitespace, addresses
+whitespace = re.compile(r"\s+")
+web_address = re.compile(r"(?i)http(s):\/\/[a-z0-9.~_\-\/]+")
+
+
+#------------------------------------------------------------------------------#
+
+#PART 3: SENTIMENT ANALYSIS
+
+myModel = flair.models.TextClassifier.load('en-sentiment')
+
+probabilities = []  #confidence of prediction 
+sentiments = []     #POSITIVE/NEGATIVE
+
+for tweet in tweets['text'].to_list():
+    #preprocess tweet
+    tweet = whitespace.sub(' ', tweet)
+    tweet = web_address.sub('', tweet)
+
+    #predict the pos/neg
+    sentence = flair.data.Sentence(tweet)
+    myModel.predict(sentence)
+    
+    #add prediction values
+    probabilities.append(sentence.labels[0].score)  
+    sentiments.append(sentence.labels[0].value) 
+
+# add probability and sentiment predictions to tweets dataframe
+tweets['probability'] = probabilities
+tweets['sentiment'] = sentiments
+
+
+
