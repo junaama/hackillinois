35f3c119504ab453ce2d13ff91772b88f38387d4
Jeffrey Sun <jeffreys2002@gmail.com>
2021-04-09 23:16:30 -0500

    Filter for Daily Discussion and GME Megathread

--- reddit.py
+++ reddit.py
@@ -8,8 +8,20 @@ reddit = praw.Reddit(client_id='RgFYwRpBqsZ5vw', client_secret='1QLaOziV8n77kzEQ
 # print(reddit.read_only)
 
 # simple demonstration
-for submission in reddit.subreddit('wallstreetbets').hot(limit=10):
+# for submission in reddit.subreddit('wallstreetbets').top(limit=1000):
+#     if submission.link_flair_text == 'Daily Discussion':
+#         print(submission.title)
+#         print(submission.link_flair_text)
+
+for submission in reddit.subreddit('wallstreetbets').search('flair:"Daily Discussion"', limit=10):
+    print(submission.title)
+    print(submission.link_flair_text)
+
+for submission in reddit.subreddit('wallstreetbets').search(query='GME Megathread', sort='new', limit=2):
     print(submission.title)
 
-url = 'https://www.reddit.com/r/wallstreetbets/comments/mnf6e4/gme_megathread_for_april_09_2021/'
-submission = reddit.submission(url=url)
+# url = 'https://www.reddit.com/r/wallstreetbets/comments/mnf6e4/gme_megathread_for_april_09_2021/'
+# submission = reddit.submission(url=url)
+#
+# for top_level_comment in submission.comments:
+#     print(top_level_comment)
