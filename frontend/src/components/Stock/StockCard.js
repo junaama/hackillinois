1fafb87536e8975c562502da65837b4a84dc30ba
jgutie45 <jgutie45@uic.edu>
2021-04-10 23:55:51 -0500

    Added Search Drop Box, Updated Dashboard, and Card Component

--- frontend/src/components/Stock/StockCard.js
+++ frontend/src/components/Stock/StockCard.js
@@ -1,30 +1,37 @@
 //**Renders each invidual Stock Card data */
 import React from 'react';
 import './StockCard.css'
+import stockData from '../../stockdata.json'
 
-function Card({CompanyName, CompanyAbr, Recommend, Sentiment, ConfidenceScore}){
+function Card(props){
+    const Response= stockData.filter((item) =>{
+        return item["Name"] === props.ticker
+    });
+
+    console.log(Response);
     return(
         <div className="card-container">
-
             <div className="card-content">
                 <div className="card-title">
-                    <h1>{CompanyName}</h1>
+                    <h3>{Response[0]["Name"]}</h3>
                 </div>
                 <div className="card-abrv">
-                    <p>{CompanyAbr}</p>
+                    <p>{Response[0]["Ticker"]}</p>
                 </div>
                 <div className="card-recommend">
-                    <p>{Recommend}</p>
+                    <p>{Response[0]["Recommendation"]}</p>
                 </div>
                 <div className="card-sentiment">
-                    <p>{Sentiment}</p>
+                    <p>{Response[0]["Sentiment"]}</p>
                 </div>
                 <div className="card-score">
-                    <p>{ConfidenceScore}</p>
+                    <p>{Response[0]["Confidence Score"]}</p>
                 </div>
             </div>
-
+            <div>
+  </div>
     </div>
+
     )
 }
 
