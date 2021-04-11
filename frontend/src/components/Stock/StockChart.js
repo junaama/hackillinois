d342e3f2ed59cedd5a36965b01c43358df51725c
jgutie45 <jgutie45@uic.edu>
2021-04-11 14:39:11 -0500

    Updated Stock Card

--- frontend/src/components/Stock/StockChart.js
+++ frontend/src/components/Stock/StockChart.js
@@ -1,1 +1,20 @@
 //**Renders Stock Chart */
+import React from "react";
+
+function Graph(){
+    return(
+<div className="bg-blue-800 text-white py-5 px-5">
+  <div className="flex flex-wrap items-end">
+    <div className="flex-1">
+      <h3 className="text-lg font-semibold leading-tight">Stocks Graph</h3>
+    </div>
+  </div>
+  <div>
+    <canvas id="chart" className="bg-blue-800"></canvas>
+  </div>
+</div>
+
+    
+    );
+}
+export default Graph;
