17018823d067a683af7f41f49c9e3acff522e423
jgutie45 <jgutie45@uic.edu>
2021-04-10 20:42:58 -0500

    Updated StockCard Component

--- frontend/src/components/Stock/StockCard.js
+++ frontend/src/components/Stock/StockCard.js
@@ -1,1 +1,31 @@
 //**Renders each invidual Stock Card data */
+import React from 'react';
+import './StockCard.css'
+
+function Card({CompanyName, CompanyAbr, Recommend, Sentiment, ConfidenceScore}){
+    return(
+        <div className="card-container">
+
+            <div className="card-content">
+                <div className="card-title">
+                    <h1>{CompanyName}</h1>
+                </div>
+                <div className="card-abrv">
+                    <p>{CompanyAbr}</p>
+                </div>
+                <div className="card-recommend">
+                    <p>{Recommend}</p>
+                </div>
+                <div className="card-sentiment">
+                    <p>{Sentiment}</p>
+                </div>
+                <div className="card-score">
+                    <p>{ConfidenceScore}</p>
+                </div>
+            </div>
+
+    </div>
+    )
+}
+
+export default Card
