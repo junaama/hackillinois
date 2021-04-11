2aa30fc15f485e9f3ee297ab63003620224b2b77
junaama <0naama0@gmail.com>
2021-04-11 17:26:33 -0400

    removed fontawesome import

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import '@fortawesome/fontawesome-free/css/all.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter as Router} from 'react-router-dom';
